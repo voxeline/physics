5229a790013f6ae7b37681502114d158aa42c37a
Giff Song <giff.song@gmail.com>
2016-10-14 16:46:51 +0900

    [utils] Use gl-matrix API

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
+import vec3 = require('gl-matrix/src/gl-matrix/vec3');
 import AABB from './AABB';
 
 export function equals(a: number, b: number) {
@@ -5,9 +6,8 @@ export function equals(a: number, b: number) {
 }
 
 export function cloneAABB(tgt: AABB, src: AABB) {
-  for (let i = 0; i < 3; i++) {
-    tgt.base[i] = src.base[i];
-    tgt.max[i] = src.max[i];
-    tgt.size[i] = src.size[i];
-  }
+  vec3.copy(tgt.base, src.base);
+  vec3.copy(tgt.max, src.max);
+  vec3.copy(tgt.size, src.size);
+  vec3.copy(tgt.area, src.area);
 }
